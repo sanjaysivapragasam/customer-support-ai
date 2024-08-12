2cd3ddf84771609d4f30a20f5d64be2a8f17180f
Sanjay Sivapragasam <sanjaysivapragasam12@gmail.com>
2024-08-11 22:47:34 -0400

    Added markdown parsing for AI output

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import axios from 'axios';
-//const APIKEY = process.env.GOOGLE_API_KEY;
+const APIKEY = process.env.GOOGLE_API_KEY;
 
 // Define the system prompt for the AI assistant
 const systemPrompt = `You are a Walmart Tech Support Agent. Please try to answer all questions and queries as best as possible`;
@@ -10,14 +10,14 @@ export async function POST(req) {
     const data = await req.json();
     const { GoogleGenerativeAI } = require("@google/generative-ai");
   
-    const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+    const genAI = new GoogleGenerativeAI(APIKEY);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
     const result = await model.generateContent(systemPrompt + "\n" + data.map(message => `${message.role}: ${message.content}`).join("\n"));
     const response = await result.response;
     const text = await response.text();
     console.log(text);
-    const cleanedText = text.replace("assistant: ", "").replace(/\n$/, "");
-    return new NextResponse(cleanedText, {
+   // const cleanedText = text.replace("assistant: ", "").replace(/\n$/, "");
+    return new NextResponse(text, {
       headers: {
         'Content-Type': 'text/plain'
       }
